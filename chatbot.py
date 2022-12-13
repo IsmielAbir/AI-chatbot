e4cd040c6fc6281c2869f0c82e6fcd73f37e5ce7
Ismielabir <ismielabir286@gmail.com>
2022-12-13 20:38:17 +0600

    updated

--- chatbot.py
+++ chatbot.py
@@ -9,7 +9,7 @@ from keras.models import load_model
 
 lemmatizer = WordNetLemmatizer()
 intents = json.loads(open('intents.json').read())
-
+name = "Alexa"
 words = pickle.load(open('words.pkl', 'rb'))
 classes = pickle.load(open('classes.pkl', 'rb'))
 model = load_model('chatbotmodel.h5')
